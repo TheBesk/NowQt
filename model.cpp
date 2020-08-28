0ba0bc61fd3a39983101cf2ffa223b6ae8c1466a
fedeconsa <federico.consalvo@studenti.unipd.it>
2020-08-28 20:32:40 +0200

    aggiunto controllocliente per i clienti doppi

--- model.cpp
+++ model.cpp
@@ -10,7 +10,6 @@ model::~model()
 }
 
 /*
-
 QStringList model::getCampiCliente(const unsigned int indice) const
 {
     QStringList temp;
@@ -95,58 +94,76 @@ QStringList model::getListaClientiT(QMap< int,  int>& indexMapper) const
     if(!datiTotali->empty()){
         while(it!=datiTotali->end()){
             cliente = (QString::fromStdString((*(*it)).getNome() + " " + (*(*it)).getCognome()));
-                indexMapper.insert((uint)ret.count(),count);
-
-                ret.push_back(cliente);
+            indexMapper.insert((uint)ret.count(),count);
+            ret.push_back(cliente);
             count++;
             ++it;
         }
     }
-
     return ret;
+}
+
+bool model::controlloCliente(const QString cf) const {
+    QString codf;
+    auto it=datiTotali->begin();
+    if(!datiTotali->empty()){
+        while(it!=datiTotali->end()){
+            codf = (QString::fromStdString((*(*it)).getCodFisc()));
+            if(cf==codf) {
+                return false;
+            }
+            ++it;
+        }
+        return true;
+    }
+    else return true;
 }
 
 void model::aggNelContainer(const QStringList c)
 {
     modificato=true;
     deepPointer<nowqt> cliente;
-    QDate dataNascitaTemp = QDate::fromString(c.at(3));
-    QDate dataInizioAbbTemp = QDate::fromString(c.at(25));
-    if ((c.at(5) == "true" && c.at(11) == "true") && (c.at(17) == "true")) {
-        cliente = new allinclusive(c.at(0).toStdString(),c.at(1).toStdString(), dataNascitaTemp.year(),dataNascitaTemp.month(),dataNascitaTemp.day(),c.at(2).toStdString(),
-                                   c.at(4).toStdString(), dataInizioAbbTemp.year(), dataInizioAbbTemp.month(),dataInizioAbbTemp.day(), c.at(26).toUInt(),
-                                   c.at(23)=="true" ? true:false, c.at(24).toStdString(),c.at(27).toDouble(), c.at(6).toStdString(), c.at(7).toStdString(), c.at(8).toDouble(),
-                                   c.at(9).toUInt(), c.at(9).toUInt(), c.at(10)=="true" ? true:false, c.at(12).toStdString(), c.at(13).toStdString(), c.at(14).toDouble(),
-                                   c.at(15).toUInt(), c.at(15).toUInt(), c.at(16)=="true" ? true:false, c.at(18).toStdString(), c.at(19).toStdString(), c.at(20).toDouble(),
-                                    c.at(21).toUInt(), c.at(21).toUInt(), c.at(22)=="true" ? true:false, c.at(27).toDouble());
-    } // solo kids
-
-    else if((c.at(5) == "true") && (c.at(11) == "false" && c.at(17) == "false")){
-        cliente = new kids(c.at(0).toStdString(), c.at(1).toStdString(), dataNascitaTemp.year(), dataNascitaTemp.month(), dataNascitaTemp.day(),
-                           c.at(2).toStdString(),c.at(4).toStdString(),dataInizioAbbTemp.year(), dataInizioAbbTemp.month(),
-                           dataInizioAbbTemp.day(), c.at(26).toUInt(), c.at(23)=="true" ? true:false, c.at(24).toStdString(), c.at(27).toDouble(),
-                           c.at(6).toStdString(), c.at(7).toStdString(), c.at(8).toDouble(), c.at(9).toUInt(), c.at(9).toUInt(), c.at(10)=="true" ? true:false);
-    } // solo cinema
-    else if((c.at(11) == "true") && (c.at(5) == "false" && c.at(17) == "false")){
-        cliente = new cinema(c.at(0).toStdString(),c.at(1).toStdString(), dataNascitaTemp.year(),dataNascitaTemp.month(),dataNascitaTemp.day(),c.at(2).toStdString(),
-                             c.at(4).toStdString(),dataInizioAbbTemp.year(), dataInizioAbbTemp.month(),dataInizioAbbTemp.day(), c.at(26).toUInt(),
-                             c.at(23)=="true" ? true:false, c.at(24).toStdString(),c.at(27).toDouble(), c.at(12).toStdString(), c.at(13).toStdString(), c.at(14).toDouble(),
-                             c.at(15).toUInt(), c.at(15).toUInt(), c.at(16)=="true" ? true:false);
-    } // solo sport
-    else if((c.at(17) == "true") && (c.at(5) == "false" && c.at(11) == "false")){
-    cliente= new sport(c.at(0).toStdString(),c.at(1).toStdString(), dataNascitaTemp.year(),dataNascitaTemp.month(),dataNascitaTemp.day(),c.at(2).toStdString(),
-            c.at(4).toStdString(), dataInizioAbbTemp.year(), dataInizioAbbTemp.month(), dataInizioAbbTemp.day(), c.at(26).toUInt(),
-            c.at(23)=="true" ? true:false, c.at(24).toStdString(),c.at(27).toDouble(), c.at(18).toStdString(), c.at(19).toStdString(), c.at(20).toDouble(),
-                      c.at(21).toUInt(), c.at(21).toUInt(), c.at(22)=="true" ? true:false);
+    QString cf = QString::fromStdString(c.at(2).toStdString());
+    if (!controlloCliente(cf)) {
+        throw new std::runtime_error("Errore: Il cliente che si sta tentando di inserire è già presente nella lista dei clienti. Riprovare.");
     }
-
-    datiTotali->push_back(cliente);
-    emit clienteAggiunto();
+    else {
+        QDate dataNascitaTemp = QDate::fromString(c.at(3));
+        QDate dataInizioAbbTemp = QDate::fromString(c.at(25));
+        if ((c.at(5) == "true" && c.at(11) == "true") && (c.at(17) == "true")) {
+            cliente = new allinclusive(c.at(0).toStdString(),c.at(1).toStdString(), dataNascitaTemp.year(),dataNascitaTemp.month(),dataNascitaTemp.day(),c.at(2).toStdString(),
+                                       c.at(4).toStdString(), dataInizioAbbTemp.year(), dataInizioAbbTemp.month(),dataInizioAbbTemp.day(), c.at(26).toUInt(),
+                                       c.at(23)=="true" ? true:false, c.at(24).toStdString(),c.at(27).toDouble(), c.at(6).toStdString(), c.at(7).toStdString(), c.at(8).toDouble(),
+                                       c.at(9).toUInt(), c.at(9).toUInt(), c.at(10)=="true" ? true:false, c.at(12).toStdString(), c.at(13).toStdString(), c.at(14).toDouble(),
+                                       c.at(15).toUInt(), c.at(15).toUInt(), c.at(16)=="true" ? true:false, c.at(18).toStdString(), c.at(19).toStdString(), c.at(20).toDouble(),
+                                        c.at(21).toUInt(), c.at(21).toUInt(), c.at(22)=="true" ? true:false, c.at(27).toDouble());
+        } // solo kids
+        else if((c.at(5) == "true") && (c.at(11) == "false" && c.at(17) == "false")){
+            cliente = new kids(c.at(0).toStdString(), c.at(1).toStdString(), dataNascitaTemp.year(), dataNascitaTemp.month(), dataNascitaTemp.day(),
+                               c.at(2).toStdString(),c.at(4).toStdString(),dataInizioAbbTemp.year(), dataInizioAbbTemp.month(),
+                               dataInizioAbbTemp.day(), c.at(26).toUInt(), c.at(23)=="true" ? true:false, c.at(24).toStdString(), c.at(27).toDouble(),
+                               c.at(6).toStdString(), c.at(7).toStdString(), c.at(8).toDouble(), c.at(9).toUInt(), c.at(9).toUInt(), c.at(10)=="true" ? true:false);
+        } // solo cinema
+        else if((c.at(11) == "true") && (c.at(5) == "false" && c.at(17) == "false")){
+            cliente = new cinema(c.at(0).toStdString(),c.at(1).toStdString(), dataNascitaTemp.year(),dataNascitaTemp.month(),dataNascitaTemp.day(),c.at(2).toStdString(),
+                                 c.at(4).toStdString(),dataInizioAbbTemp.year(), dataInizioAbbTemp.month(),dataInizioAbbTemp.day(), c.at(26).toUInt(),
+                                 c.at(23)=="true" ? true:false, c.at(24).toStdString(),c.at(27).toDouble(), c.at(12).toStdString(), c.at(13).toStdString(), c.at(14).toDouble(),
+                                 c.at(15).toUInt(), c.at(15).toUInt(), c.at(16)=="true" ? true:false);
+        } // solo sport
+        else if((c.at(17) == "true") && (c.at(5) == "false" && c.at(11) == "false")){
+        cliente= new sport(c.at(0).toStdString(),c.at(1).toStdString(), dataNascitaTemp.year(),dataNascitaTemp.month(),dataNascitaTemp.day(),c.at(2).toStdString(),
+                c.at(4).toStdString(), dataInizioAbbTemp.year(), dataInizioAbbTemp.month(), dataInizioAbbTemp.day(), c.at(26).toUInt(),
+                c.at(23)=="true" ? true:false, c.at(24).toStdString(),c.at(27).toDouble(), c.at(18).toStdString(), c.at(19).toStdString(), c.at(20).toDouble(),
+                          c.at(21).toUInt(), c.at(21).toUInt(), c.at(22)=="true" ? true:false);
+        }
+        datiTotali->push_back(cliente);
+        emit clienteAggiunto();
+    }
 }
 
 void model::removeC(const int i)
 {
     modificato=true;
-        datiTotali->erase_pos(i);
-        emit clienteRimosso();
+    datiTotali->erase_pos(i);
+    emit clienteRimosso();
 }
